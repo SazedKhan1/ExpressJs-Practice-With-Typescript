69e7b8e0319ae2895d401997fd6f9c3c741361cb
Md: Sazed Khan <manthedark233@gmail.com>
2023-11-23 23:21:12 +0600

    successfully added file

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { StudentRoute } from './app/modules/student/student.route';
 
 const app: Application = express();
 
@@ -7,9 +8,13 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
+// appliction routes
+app.use('/api/v1/students', StudentRoute);
+
 app.get('/', (req: Request, res: Response) => {
   const a = 10;
-  res.send(a);
+  const b = 100;
+  res.send({ a, b });
 });
 
 export default app;
